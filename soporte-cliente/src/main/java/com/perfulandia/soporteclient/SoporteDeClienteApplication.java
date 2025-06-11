59a93e47cc24d774b8becd5a62e7c9d1a369b745
Gabriel T <gab.toledo@duocuc.cl>
2025-06-10 22:22:33 -0400

    Inicializa proyecto >> - add file README.md >> - add file .gitignore

---
+++ soporte-cliente/src/main/java/com/perfulandia/soporteclient/SoporteDeClienteApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.soporteclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoporteDeClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoporteDeClienteApplication.class, args);
+	}
+
+}
---
+++ soporte-cliente/src/test/java/com/perfulandia/soporteclient/SoporteDeClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.soporteclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoporteDeClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
